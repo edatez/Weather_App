8fffe7d4ff48c467d45dc60918aee74fbf67c862
Eda Tuncer <edatezcanli@gmail.com>
2020-05-11 20:51:02 -0700

    updated the uv index and date

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,9 @@
 var apiId = "e9885f935a30d807d367fb450e9d819f";
 var currentDate = new Date().toLocaleDateString()
 console.log(currentDate)
-
+var city=""
 // read localstorage and build the list of the history 
+/*creating an array to the search city name, making a variable*/
 var citiesStore = JSON.parse(localStorage.getItem('weatherByCity'));
 
 if (!citiesStore) {
@@ -40,36 +41,44 @@ function searchCity(city) {
     method: "GET"
   }).then(function (response) {
     console.log(response);
-    //       for uv:  https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&
-    // exclude=hourly,daily&appid={YOUR API KEY}
-    //Display the UV data
-
-
-    //  function render and another ajax for 
-    https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid={YOUR API KEY}
-
-    // building the card with the info and appending it to that area.
-    var card = `<div class="card" style="width: 18rem;">
-       <div class="card-body">
-         <h5 class="card-title">${city}</h5>
+    calculateUV(response)
  
-         <p class="card-text">Temperature: ${response.main.temp}°F</p>
-         <p class="card-text">Humidity: ${response.main.humidity}%</p>  
-         <p class="card-text">Wind Speed: ${response.wind.speed}mph</p>
-         <p class="card-text">UV Index:${response.wind.speed} mph</p>
-
-      
-       </div>
-     </div>`
-    $("#current").empty()
-    $("#current").append(card);
-    forecast(city)
   });
 }
 // If you split first with space. The first part of the array will be the day. Then you split the first part with ‘-‘ Then you can get every piece and create a string with the format you want
 // Server Interaction -getting 5day forecast for the city entered.
 // // //  ToDos: eliminate the year at the forecast, for temps-change kelvin to F.Add a sun icon.
 // if you put :  &units=imperial in the query url the units will be in imperial units and you won't have to run a kelvin to imperial calculation
+
+function calculateUV(response) {
+  var queryParams = $.param({
+    appid: apiId,
+    lat: response.coord.lat,
+    lon: response.coord.lon
+  })
+  var queryUrl = "https://api.openweathermap.org/data/2.5/uvi?" + queryParams
+  $.ajax({
+    url: queryUrl,
+    method: "GET",
+
+  }).then(function (uvData) {
+    console.log(uvData)
+
+    var card = `<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">${response.name}</h5>
+
+    <p class="card-text">Temperature: ${response.main.temp}°F</p>
+    <p class="card-text">Humidity: ${response.main.humidity}%</p>  
+    <p class="card-text">Wind Speed: ${response.wind.speed}mph</p>
+    <p class="card-text">UV Index: ${uvData.value}</p>
+  </div>
+</div>`
+    $("#current").empty()
+    $("#current").append(card);
+    forecast(response.name)
+  })
+}
 
 function forecast(city) {
   var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiId}&units=imperial`
@@ -84,13 +93,22 @@ function forecast(city) {
     $("#forecast").empty();
     for (var i = 0; i < response.list.length; i++) {
       let day = response.list[i]
+      console.log(day);
       if (day.dt_txt.includes("12:00:00")) {
         // showing the info in the forcecast area-picking here one of them because it gives couple other choices with different times, so I picked 12:00
         //looping and creating 5 cards
+        var date = day.dt_txt.split(" ");
+        //var date = response.list[i].dt_text.split(" ");
+        var splitDate = date[0].split("-");
+        var month = splitDate[1];
+        var d = splitDate[2];
+        var y = splitDate[0];
+
+
         var card = `<div class="card col" style="width: 18rem;">
      <div class="card-body"  id="forecast">
-       <h5 class="card-title">${day.dt_txt.split()[0]}</h5>
-       <h6 class="card-subtitle mb-2 text-muted">${day.dt_txt.split("")[0]}</h6>
+       <h5 class="card-title"></h5>
+       <h6 class="card-subtitle mb-2 text-muted">${month}\\${d}\\${y}</h6>
        <p class="card-text">Temp:${day.main.temp}°F</p>
        <p class="card-text">Humidity: ${day.main.humidity}%</p> 
      </div>
